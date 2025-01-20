7499f25ff8fd559c47ed2df2c3828fca91b6461d
faruk-guler <95317318+faruk-guler@users.noreply.github.com>
2025-01-20 10:01:21 +0300

    Update repos.d

--- repos.d
+++ repos.d
@@ -8,6 +8,5 @@
 /var/lib/apt/extended_states: Paketlerin manuel mi yoksa bağımlılık olarak mı yüklendiğini tutar.
 /var/lib/dpkg/: Yüklü paketlerin veritabanı.
 /var/lib/dpkg/status: Yüklü paketlerin durum bilgisini içerir.
-/var/cache/apt/archives/: İndirilen .deb paketlerinin önbelleği.
 /var/cache/apt/archives/partial/: Henüz tamamlanmamış paket indirme işlemleri burada saklanır.
 /var/log/apt/: APT işlemleriyle ilgili log dosyalarını içerir (ör. history.log).
