43ac12df1561ab7c9c23c30c71e6a9340798d747
faruk-guler <95317318+faruk-guler@users.noreply.github.com>
2025-02-24 13:07:11 +0300

    Update system.d

--- system.d
+++ system.d
@@ -5,10 +5,10 @@ Ayrıca log yönetimi (journald), zamanlanmış görevler (timer birimleri) ve k
 
 --------------------------------------
 |Unit Türü	| Uzantı  | Açıklama |
-Service	  .service	Servisleri yönetir (ör: NGINX, MySQL).
-Socket	  .socket	Ağ veya IPC soketlerini tanımlar. Bağlantı geldiğinde ilgili servisi başlatır.
-Target	  .target	Hedef grupları (run level benzeri). Örn: multi-user.target.
-Device	  .device	Donanım cihazlarını (USB, disk) sisteme tanıtır.
+Service	    .service	Servisleri yönetir (ör: NGINX, MySQL).
+Socket	    .socket	Ağ veya IPC soketlerini tanımlar. Bağlantı geldiğinde ilgili servisi başlatır.
+Target	    .target	Hedef grupları (run level benzeri). Örn: multi-user.target.
+Device	    .device	Donanım cihazlarını (USB, disk) sisteme tanıtır.
 Mount	    .mount	Dosya sistemi mount işlemlerini yönetir (ör: /home bağlama).
 Automount	.automount Otomatik mount işlemleri için (bağlantı noktasına erişimde tetiklenir).
 Swap	    .swap	Swap alanını yönetir (takas dosyası/partition).
@@ -16,8 +16,8 @@ Timer	    .timer	Zamanlanmış görevler (cron benzeri).
 Path	    .path	Dosya/dizin değişikliklerini izler ve bir servisi tetikler.
 Slice	    .slice	Kaynak kısıtlaması için kayali "dilimler" (CPU, RAM kontrolü).
 Scope	    .scope	Dış proses gruplarını izler (genellikle container'lar için).
-Snapshot	.snapshot	Sistemin anlık durumunu kaydeder (geri yükleme için).
+Snapshot    .snapshot	Sistemin anlık durumunu kaydeder (geri yükleme için).
 Link	    .link	Network interface'lerini yapılandırır (ör: MAC adresi atama).
-Netdev	  .netdev	Sanal ağ cihazları oluşturur (ör: bridge, VLAN).
-Network	  .network	Ağ yapılandırmasını tanımlar (IP, DNS, routing).
+Netdev	    .netdev	Sanal ağ cihazları oluşturur (ör: bridge, VLAN).
+Network	    .network	Ağ yapılandırmasını tanımlar (IP, DNS, routing).
 .............
