cbba0d81b3587d1b8eafab5f2da2ddd9a86a112f
faruk-guler <95317318+faruk-guler@users.noreply.github.com>
2025-06-22 00:08:31 +0300

    Update Firewall.d

--- Firewall/Firewall.d
+++ Firewall/Firewall.d
@@ -3,5 +3,12 @@
 ############################################
 
 sudo dnf install firewalld
+sudo systemctl status firewalld
+firewalld-cmd --stat
 sudo systemctl enable firewalld
 sudo systemctl start firewalld
+systemctl stop firewalld
+systemctl restart firewalld
+
+  
+
